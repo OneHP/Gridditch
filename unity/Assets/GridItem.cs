71783ea5c2ce523494d1633a194b4c77f604a420
U-TheTurk\Thomas <Thomas@TheTurk.(none)>
2012-05-24 23:02:57 +0100

    GridItem

---
+++ unity/Assets/GridItem.cs
@@ -0,0 +1,45 @@
+using System;
+
+namespace AssemblyCSharp
+{
+	public class GridItem
+	{
+		
+		private static int DEFAULT_HEALTH = 2;
+		
+		private int value;
+		private GridItemType type;
+		private int health;
+		
+		private GridItem (int value, GridItemType type, int health)
+		{
+			this.value = value;
+			this.type = type;
+			this.health = health;
+		}
+		
+		public static GridItem createEmpty(){
+			return new GridItem(0,GridItemType.EMPTY,0);
+		}
+		
+		public static GridItem createActive(int value){
+			return new GridItem(value,GridItemType.ACTIVE,0);
+		}
+		
+		public static GridItem createInactive(){
+			return new GridItem(0,GridItemType.INACTIVE,DEFAULT_HEALTH);
+		}
+		
+		
+	}
+	
+	enum GridItemType{
+		
+		ACTIVE,
+		
+		INACTIVE,
+		
+		EMPTY
+	}
+}
+
